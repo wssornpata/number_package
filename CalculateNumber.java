1bd2bd3763b649360bc5541dff0fb2d313010b36
AzureAD\Employee0724 <sornpata@wisesoft.co.th>
2024-08-20 11:48:38 +0700

    remove static java.lang

--- CalculateNumber.java
+++ CalculateNumber.java
@@ -1,6 +1,5 @@
 package number_package;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
--- ex1.java
+++ ex1.java
@@ -1,7 +1,5 @@
 package number_package;
 
-import static java.lang.System.out;
-
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -15,21 +13,21 @@ public class ex1 {
 
         while (loop) {
             try {
-                out.print("Enter numbers (Split by \",\" symbol): ");
+                System.out.print("Enter numbers (Split by \",\" symbol): ");
                 String input = sc.nextLine();
                 numbers = Arrays.stream(input.split(","))
                         .map(String::trim)
                         .filter(s -> !s.isEmpty())
                         .mapToInt(Integer::parseInt)
                         .toArray();
-                out.print("Target: ");
+                        System.out.print("Target: ");
                 target = sc.nextInt();
             } catch (NumberFormatException e) {
-                out.println("Warning : Value is not numeric number.");
+                System.out.println("Warning : Value is not numeric number.");
                 continue;
             }
             obj.setData(numbers, target);
-            out.println(Arrays.toString(obj.calculate()));
+            System.out.println(Arrays.toString(obj.calculate()));
         }
         // numbers = new int[] { 2, 2, 7, 7, 11, 15 };
         // target = 9;
